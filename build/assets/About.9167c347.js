e41ea705cd25b1c0e38c57da8c55c38f2536e79b
John Pangalos <john.pangalos@skf.com>
2021-06-30 13:51:33 +0200

    Remove build from repo

--- build/assets/About.9167c347.js
+++
@@ -1,2 +0,0 @@
-import{R as e}from"./react.c5d4f650.js";import{_ as t}from"./lodash.286132a0.js";function n(){const n=t.join(["a","b","c"],"~");return e.createElement(e.Fragment,null,e.createElement("h2",null,"About"),e.createElement("div",null,n))}export default n;
-//# sourceMappingURL=About.9167c347.js.map
